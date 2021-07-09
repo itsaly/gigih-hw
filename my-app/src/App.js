8d288e60a9f162f799dfc64e46492ec2e9164911
Alya Mizani <entschuldigung.itsalya@gmail.com>
2021-07-09 23:07:39 +0700

    hands on module 1 session 3

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const giphy_secret_key = process.env.REACT_APP_GIPHY_KEY;
+  console.log(giphy_secret_key);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
