1893b837b554d4ae0245fc55b3fd2be5b09b02ee
Alya Mizani <entschuldigung.itsalya@gmail.com>
2021-07-13 13:19:10 +0700

    hands on module 2 session 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Layout from './pages/home/index';
 
 function App() {
   const giphy_secret_key = process.env.REACT_APP_GIPHY_KEY;
   console.log(giphy_secret_key);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout />
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
